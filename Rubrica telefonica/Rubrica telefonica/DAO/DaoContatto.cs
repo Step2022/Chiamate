bca6a6c5983f8cd2f7a0bed8fe7db64ccb1dbbd3
Alessandro Tedeschi <alessandro.tedeschi@stepsrl.org>
2022-07-22 15:03:59 +0200

    commmit Dao contatto metodi

--- Rubrica telefonica/Rubrica telefonica/DAO/DaoContatto.cs
+++ Rubrica telefonica/Rubrica telefonica/DAO/DaoContatto.cs
@@ -11,5 +11,69 @@ namespace Rubrica_telefonica.DAO
         {
             _context = context;
         }
+        public bool AddContatto(Contatto contatto)
+        {
+            try
+            {
+                if (contatto != null)
+                {
+
+                    _context.Contattos.Add(contatto);
+                    _context.SaveChanges();
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+
+            }
+
+        }
+
+        public Contatto GetContatto(int idContatto, int idPropietario)
+        {
+           return _context.Contattos.FirstOrDefault(x => x.IdContatto == idContatto && x.IdPropietario==idPropietario);
+        }
+        public List<Contatto> GetContatti(int idNumero)
+        {
+            return _context.Contattos.Where(x => x.IdPropietario == idNumero).ToList();
+        }
+
+       /* public bool EditContatto(int idContatto, int idPropietario)
+        {
+            try{
+
+            }catch ()
+            {
+
+            }
+        }*/
+      
+        public bool RemoveContatto(int idContatto, int idPropietario)
+        {
+            try
+            {
+                _context.Contattos.Remove(GetContatto(idContatto,idPropietario));
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+
+
+        /*AddContatto(idNumero)
+GetContatto(idContatto)
+GetContatti(idNumero)
+ModificaContatto(idContatto)
+EliminaContatto(idContatto)*/
+
     }
 }
