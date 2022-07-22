efcba430b273c884e3329685e0c615a50fbc2fb7
Matteo Napoli <m.napoli-ext@stepsrl.org>
2022-07-22 09:58:23 +0200

    Creazione progetto

---
+++ Rubrica telefonica/Rubrica telefonica/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Rubrica_telefonica.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
